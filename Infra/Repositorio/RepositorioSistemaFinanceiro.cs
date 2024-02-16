fd042361e9878863e6d80e1958122fbe4f5a0b3a
Arthur Caetano <arthurcaetano.acrr@gmail.com>
2024-02-16 17:43:56 -0300

    Adicionar arquivos de projeto.

---
+++ Infra/Repositorio/RepositorioSistemaFinanceiro.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Interfaces.ISistemaFinanceiro;
+using Domain.Interfaces.IDespesa;
+using Entities.Entidades;
+using Infra.Repositorio.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infra.Configuracao;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositorio
+{
+    public class RepositorioSistemaFinanceiro : RepositoryGenerics<SistemaFinanceiro>, InterfaceSistemaFinanceiro
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositorioSistemaFinanceiro()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task<IList<SistemaFinanceiro>> ListaSistemasUsuario(string emailUsuario)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                return await
+                   (from s in banco.SistemaFinanceiro
+                    join us in banco.UsuarioSistemaFinanceiro on s.Id equals us.IdSistema
+                    where us.EmailUsuario.Equals(emailUsuario)
+                    select s).AsNoTracking().ToListAsync();
+            }
+        }
+    }
+}
