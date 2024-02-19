039be105b935967d62c2ced4ea63be707d48eaa6
Arthur Caetano <arthurcaetano.acrr@gmail.com>
2024-02-19 17:30:37 -0300

    Primeira Api do sistema financeiro 17:33

---
+++ WebApi/Controllers/UsuarioSistemaFinanceiroController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Interfaces.InterfaceServicos;
+using Domain.Interfaces.IUsuarioSistemaFinanceiro;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioSistemaFinanceiroController : ControllerBase
+    {
+        private readonly InterfaceUsuarioSistemaFinanceiro _InterfaceUsuarioSistemaFinanceiro;
+        private readonly IUsuarioSistemaFinanceiroServico _IUsuarioSistemaFinanceiroServico;
+        public UsuarioSistemaFinanceiroController(InterfaceUsuarioSistemaFinanceiro InterfaceUsuarioSistemaFinanceiro,
+            IUsuarioSistemaFinanceiroServico IUsuarioSistemaFinanceiroServico)
+        {
+            _InterfaceUsuarioSistemaFinanceiro = InterfaceUsuarioSistemaFinanceiro;
+            _IUsuarioSistemaFinanceiroServico = IUsuarioSistemaFinanceiroServico;
+        }
+
+
+        [HttpGet("/api/ListaSistemaUsuario")]
+        [Produces("application/json")]
+        public async Task<object> ListaUsuarioSistemaFinanceiro(int idSistema)
+        {
+            return await _InterfaceUsuarioSistemaFinanceiro.ListarUsuariosSistema(idSistema);
+        }
+    }
+}
