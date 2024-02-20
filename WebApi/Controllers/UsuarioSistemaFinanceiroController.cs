eca7dee5a2ae5c54a143f918a4d94b492144eb2f
Arthur Caetano <arthurcaetano.acrr@gmail.com>
2024-02-20 14:35:06 -0300

    Usuarios do Sistema criados 22:55

--- WebApi/Controllers/UsuarioSistemaFinanceiroController.cs
+++ WebApi/Controllers/UsuarioSistemaFinanceiroController.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Interfaces.InterfaceServicos;
 using Domain.Interfaces.IUsuarioSistemaFinanceiro;
+using Entities.Entidades;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,5 +26,49 @@ namespace WebApi.Controllers
         {
             return await _InterfaceUsuarioSistemaFinanceiro.ListarUsuariosSistema(idSistema);
         }
+
+
+        [HttpPost("/api/ListaSistemaUsuario")]
+        [Produces("application/json")]
+        public async Task<object> CadastrarUsuarioNoSistema(int idSistema, string emailUsuario)
+        {
+            try
+            {
+                await _IUsuarioSistemaFinanceiroServico.CadastrarUsuarioNoSistema(
+                new UsuarioSistemaFinanceiro
+                {
+                    IdSistema = idSistema,
+                    EmailUsuario = emailUsuario,
+                    Administrador = false,
+                    SistemaAtual = true
+                });
+            }
+            catch (Exception)
+            {
+
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+
+        [HttpDelete("/api/DeleteUsuarioSistemaFinanceiro")]
+        [Produces("application/json")]
+        public async Task<object> DeleteUsuarioSistemaFinanceiro(int id)
+        {
+            try
+            {
+                var usuarioSistemaFinanceiro = await _InterfaceUsuarioSistemaFinanceiro.GetEntityById(id);
+
+                await _InterfaceUsuarioSistemaFinanceiro.Delete(usuarioSistemaFinanceiro);
+            }
+            catch (Exception)
+            {
+
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
     }
 }
